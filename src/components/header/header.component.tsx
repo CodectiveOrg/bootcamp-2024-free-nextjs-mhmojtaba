9a40e3dbcda51945024b681edd10d43d866f69d9
Mojtaba Mohammad <mhmojtaba@gmail.com>
2024-12-20 17:40:15 +0330

    [ADD] header component

---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { ReactElement } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import clsx from "clsx";
+
+import { headerMenuItems } from "@/constants/constants";
+import styles from "./header.module.css";
+
+export default function HeaderComponent(): ReactElement {
+  const pathname = usePathname();
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul>
+          {headerMenuItems.map((item, index) => (
+            <li key={index}>
+              <Link
+                href={item.href}
+                className={clsx(pathname === item.href && styles.active)}
+              >
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <button className={styles.btnLogin}>ورود | ثبت نام</button>
+    </header>
+  );
+}
---
+++ src/constants/constants.ts
@@ -0,0 +1,5 @@
+export const headerMenuItems = [
+  { title: "خانه", href: "/" },
+  { title: "جستجو", href: "/search" },
+  { title: "تماس با ما", href: "/contact" },
+];
