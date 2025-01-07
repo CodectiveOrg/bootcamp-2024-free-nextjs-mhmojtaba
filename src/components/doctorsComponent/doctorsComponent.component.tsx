d50ecb4e497f50c92c52780a8c0a1e5ffcdb1518
Mojtaba Mohammad <mhmojtaba@gmail.com>
2025-01-07 23:53:02 +0330

    [ADD] pagination

--- src/components/doctorsComponent/doctorsComponent.component.tsx
+++ src/components/doctorsComponent/doctorsComponent.component.tsx
@@ -6,17 +6,30 @@ import { useDoctor } from "@/providers/doctors.provider";
 import DoctorCardComponent from "../doctor-card/doctorCard.component";
 
 import styles from "./doctorsComponent.module.css";
+import usePagination from "@/hooks/pagination";
+import clsx from "clsx";
 export default function DoctorsComponent(): ReactElement {
   const { isLoading, doctors } = useDoctor();
-
+  const { currentItems, currentPage, totalPages, setPage } = usePagination({ items: doctors, pageSize: 7 });
+  console.log(currentPage);
   if (isLoading) { return <div className={styles.loading}>loading...</div> }
 
   if (doctors.length === 0) { return <div className={styles.empty}>هیچ پزشکی یافت نشد</div> }
 
   return (
-
     <section className={styles["doctor-container"]} >
-      {doctors.map(doctor => <DoctorCardComponent key={doctor.id} doctor={doctor} />)}
+      {currentItems.map(doctor => <DoctorCardComponent key={doctor.id} doctor={doctor} />)}
+      <div className={styles.pagination}>
+        {[...Array(totalPages)].map((_, i) => (
+          <button
+            key={i + 1}
+            onClick={() => setPage(i + 1)}
+            className={clsx(styles.paginationBtn, `${i + 1 === currentPage ? styles.active : ''}`)}
+          >
+            {i + 1}
+          </button>
+        ))}
+      </div>
     </section >
   );
 }
---
+++ src/hooks/pagination.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { DoctorModel } from '@/types/types';
+import { useState, useEffect } from 'react';
+
+interface PaginationProps {
+    items: DoctorModel[];
+    pageSize: number;
+}
+
+
+function usePagination({ items, pageSize }: PaginationProps) {
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const totalPages = Math.ceil(items.length / pageSize);
+    const indexOfLastItem = currentPage * pageSize;
+    const indexOfFirstItem = indexOfLastItem - pageSize;
+
+    const currentItems: DoctorModel[] = items.slice(indexOfFirstItem, indexOfLastItem);
+    const setPage = (newPage: number) => {
+        if (newPage >= 1 && newPage <= totalPages) {
+            setCurrentPage(newPage);
+        }
+    };
+
+    useEffect(() => {
+        // Ensure currentPage doesn't exceed totalPages
+        if (currentPage > totalPages) {
+            setCurrentPage(totalPages);
+        }
+    }, [currentPage, totalPages]);
+
+    return {
+        currentItems,
+        currentPage,
+        totalPages,
+        setPage
+    };
+
+}
+
+export default usePagination;
