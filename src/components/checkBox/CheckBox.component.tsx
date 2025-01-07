d587495d00ad6c5ebea237a9204a38e93b30d9ad
Mojtaba Mohammad <mhmojtaba@gmail.com>
2025-01-08 02:52:36 +0330

    [ADD] checkbox component

---
+++ src/components/checkBox/CheckBox.component.tsx
@@ -0,0 +1,35 @@
+'use client'
+import React from 'react'
+
+import { DoctorModel } from '@/types/types';
+
+type CheckBoxProps = {
+    label: string;
+    value: string;
+}
+
+import styles from "./checkbox.module.css";
+import { useDoctor } from '@/providers/doctors.provider';
+function CheckBoxComponent({ label, value }: CheckBoxProps) {
+    const { filterDoctors } = useDoctor();
+
+    const onCheckHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.checked) {
+            filterDoctors({ keys: 'badges', value: e.target.value });
+        } else {
+            filterDoctors({ keys: 'badges', value: '' });
+        }
+    }
+
+    return (
+        <div className={styles.container}>
+            <label htmlFor={value} className={styles.mainLabel}>{label}</label>
+            <div className={styles["checkbox-wrapper"]}>
+                <input className={`${styles["tgl"]} ${styles["tgl-flat"]}`} id={value} value={value} type="checkbox" onChange={(e) => onCheckHandler(e)} />
+                <label className={styles["tgl-btn"]} htmlFor={value}></label>
+            </div>
+        </div >
+    )
+}
+
+export default CheckBoxComponent
