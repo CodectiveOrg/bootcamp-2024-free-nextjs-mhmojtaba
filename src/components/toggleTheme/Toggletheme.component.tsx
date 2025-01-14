6097ba2f02e3c8bdbbcc2d68d0edacdc6be80389
Mojtaba Mohammad <mhmojtaba@gmail.com>
2025-01-14 19:48:54 +0330

    [ADD] theme toggle

---
+++ src/components/toggleTheme/Toggletheme.component.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useTheme } from '@/providers/theme.provider';
+import MoonIcon from '@/icons/Moon';
+import SunIcon from '@/icons/Sun';
+
+const TogglethemeComponent = () => {
+    const { theme, toggleTheme } = useTheme();
+    return (
+        <>
+            <div onClick={toggleTheme} style={{ display: "flex" }}>
+                {theme === "light" ? (<SunIcon />) : (<MoonIcon />)}
+            </div>
+        </>
+    )
+}
+
+export default TogglethemeComponent
---
+++ src/providers/theme.provider.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+
+type theme = "light" | "dark";
+
+type ThemeContextType = {
+    theme: theme;
+    toggleTheme: () => void;
+};
+
+type Props = PropsWithChildren;
+
+const ThemeContext = createContext<ThemeContextType>({} as ThemeContextType);
+
+export const ThemeProvider = ({ children }: Props) => {
+    const [theme, setTheme] = useState<theme>("light");
+
+    const toggleTheme = () => {
+        setTheme((prev) => (prev === "light" ? "dark" : "light"));
+        document.body.setAttribute("data-theme", theme);
+        const htmlElement = document.querySelector('html');
+        if (htmlElement) {
+            htmlElement.style.colorScheme = theme;
+        }
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => {
+    return useContext(ThemeContext);
+};
