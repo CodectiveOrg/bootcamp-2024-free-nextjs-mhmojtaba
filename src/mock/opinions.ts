352fcf39e182916138eb002508d67d133a9fdc9a
Mojtaba Mohammad <mhmojtaba@gmail.com>
2025-01-28 18:32:19 +0330

    [ADD] opinion mock data

---
+++ src/mock/opinions.ts
@@ -0,0 +1,57 @@
+import { OpinionModel } from "@/types/types";
+
+export const opinionData: OpinionModel[] = [
+  {
+    id: "1001",
+    author: "سعید",
+    image: null,
+    isVisited: true,
+    rate: 4.7,
+    onlineVisit: true,
+    message:
+      "دکتر بسیار باتجربه و حاذقی هستندودر برخورد بامریض بسیار خوش برخوررد.",
+    lastVisit: "یک ماه قبل",
+  },
+  {
+    id: "1002",
+    author: "تقی",
+    image: null,
+    isVisited: true,
+    rate: 6,
+    onlineVisit: false,
+    message: "برخوردوتشخیص پزشک عالی، نظم‌مطب هم عالی بود",
+    lastVisit: "یک هفته قبل",
+  },
+  {
+    id: "1003",
+    author: "مرضیه",
+    image: null,
+    isVisited: true,
+    rate: 7.5,
+    onlineVisit: false,
+    message:
+      "اولین تجربه بنده بود و از نحوه برقراری ارتباط راضی بودم خیلی در کاهش اتلاف وقت به بیمار کمک میکنه",
+    lastVisit: "دو ماه قبل",
+  },
+  {
+    id: "1004",
+    author: "الهام",
+    image: null,
+    isVisited: true,
+    rate: 4.9,
+    onlineVisit: false,
+    message:
+      "همه چیز عالی بود فقطدر مطب معطل شدم. به علت شلوغی وپذیرش بیش از حد",
+    lastVisit: "سه هفته قبل",
+  },
+  {
+    id: "1005",
+    author: "محمد",
+    image: null,
+    isVisited: true,
+    rate: 6.3,
+    onlineVisit: true,
+    message: "عالی و خوش برخورد ویزیت کردن سریع و راهنمایی خیلی عالی",
+    lastVisit: "پنج روز قبل",
+  },
+];
